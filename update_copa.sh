54dc8e1f77fa45138cc3ab5f85d01c5bdbcdac40
fricodelco <rodion_anisimov@mail.ru>
2022-03-24 17:07:48 +0300

    change sh

--- update_copa.sh
+++ update_copa.sh
@@ -1,9 +1,6 @@
 cd ~/copa5/ws/src/image_matching
-<<<<<<< HEAD
-git restore*
-git pull
-=======
+git restore *
 git pull
 cd ~/copa5
+git restore *
 git pull
->>>>>>> c2f0584f4ac80b8b43cd6a9b4ca07715b2beff11
